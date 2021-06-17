ec031e70464d58062136d0c852a48ae20bab78eb
Satyakoidala <51870750+Satyakoidala@users.noreply.github.com>
2021-06-17 21:00:41 +0530

    deleting directory

--- finalproject/student/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from .views import *
-
-app_name = 'student'
-
-urlpatterns = [
-    path('home/', homepageview, name='home-page-view'),
-]
--- finalproject/student/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def homepageview(request, *args, **kwargs):
-    return render(request, 'student/homepage.html', {})
