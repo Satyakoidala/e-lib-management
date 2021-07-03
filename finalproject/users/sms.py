7241009f5a4abf0639686bbd9bb224c5bde3d786
Satyakoidala <koidalasai21@gmail.com>
2021-07-03 15:07:48 +0530

    final version

--- finalproject/users/sms.py
+++ finalproject/users/sms.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 
 def send_otp(phone):
     user_phone = phone
-    url = "http://2factor.in/API/V1/5d1f391e-d238-11eb-8089-0200cd936042/SMS/" + \
+    url = "http://2factor.in/API/V1/8702453c-dbe0-11eb-8089-0200cd936042/SMS/" + \
         user_phone + "/AUTOGEN/OTPSEND"
     response = requests.request("GET", url)
     data = response.json()
@@ -15,7 +15,7 @@ def send_otp(phone):
 
 def otp_verification(request, otp):
     user_otp = otp
-    url = "http://2factor.in/API/V1/5d1f391e-d238-11eb-8089-0200cd936042/SMS/VERIFY/" + \
+    url = "http://2factor.in/API/V1/8702453c-dbe0-11eb-8089-0200cd936042/SMS/VERIFY/" + \
         request.session['otp_session_data'] + "/" + user_otp + ""
     # otp_session_data is fetched from session.
     response = requests.request("GET", url)
